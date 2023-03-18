b7c07c2cad5e121cb1a63fead667fdb22f7fa494
Iggo <Iggonzioki@gmail.com>
2023-03-18 13:52:49 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'a program for combinations of single-digit numbers'
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
