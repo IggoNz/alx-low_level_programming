11a6a975010f937472e32d98ba04b5309f918955
Iggo <Iggonzioki@gmail.com>
2023-03-19 12:20:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,10 @@ int main(void)
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		if (n == 57)
+		if (n != 57)
 		{
-			continue;
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
