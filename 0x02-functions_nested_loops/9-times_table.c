d03042e7b849facb8d59083b490aad82d6686c64
Iggo <Iggonzioki@gmail.com>
2023-03-22 17:30:36 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,23 +18,23 @@ void times_table(void)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				_putcher(44);
-				_putcher(32);
-				_putcher(d + '0');
-				_putcher(u + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
 				if (y != 0)
 				{
-					_putcher(44);
-					_putcher(32);
-					_putcher(32);
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
-				_putcher(z + '0');
+				_putchar(z + '0');
 			}
 		}
 	}
-	putcher('\n');
+	putchar('\n');
 }
 
