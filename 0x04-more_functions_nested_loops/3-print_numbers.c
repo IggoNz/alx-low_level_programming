4f3795da825f909fd0660ed9e1e116496485a10d
Iggo <Iggonzioki@gmail.com>
2023-03-24 05:39:37 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * print_numbers - a function that prints the numbers,
  * from 0 to 9
- * Return: 0
+ * Return: the number
  */
 void print_numbers(void)
 {
 	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
 	_putchar(c + '0');
 	}
