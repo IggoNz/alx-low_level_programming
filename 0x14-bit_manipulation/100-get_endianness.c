c0db9f025721b708b4f99c224d586dece4cc80a9
Iggo <Iggonzioki@gmail.com>
2023-05-07 07:05:24 -0700

    task 06

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
+ * get_endianness - checks the endianness of a machine whether it is big or small
+ * Return: if the machine is big return 0, else if it is small return 1
  */
 int get_endianness(void)
 {
@@ -11,4 +11,7 @@ int get_endianness(void)
 
 	return (*c);
 }
+/**
+ * This code is used to check the endianness of a machine, which refers to the order in which data is stored in memory. If the machine is big-endian, then the data is stored with the most significant byte first, and if the machine is little-endian, then the data is stored with the least significant byte first.
 
+The code creates an unsigned integer variable, 'i', and assigns it the value of 1. The code then creates a character pointer, 'c', and assigns it the address of 'i'. The code then returns the value of the character pointed to by 'c'. If the machine is big-endian, the value returned would be 0, and if the machine is little-endian, the value returned would be 1.
