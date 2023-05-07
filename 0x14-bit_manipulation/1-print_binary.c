c04346c943ba35b5841df1d4993ea492106e87fb
Iggo <Iggonzioki@gmail.com>
2023-05-06 20:13:59 -0700

    task 02 redone

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -26,3 +26,12 @@ void print_binary(unsigned long int n)
 	if (!count)
 		_putchar ('0);
 }
+
+/**
+ * This code is used to convert a decimal number to binary.
+It starts by declaring two variables, i (which will be used as a counter) and count (which will keep track of the number of binary digits printed).
+It then enters a for loop that runs for 63 iterations, each time shifting the value of n to the right by one bit.
+The if statement checks if the shifted value of n has a one in the least significant bit position.
+If it does, the _putchar() function is used to print a '1' character and the count variable is incremented.
+If it doesn't, the _putchar() function is used to print a '0' character, but only if the count variable is greater than 0.
+Finally, if the count variable is 0, the _putchar() function is used to print a '0' character.
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
- * @index: index of the bit
- *
- * Return: value of the bit
+ * get_bit - used to get the value of a bit at a given specified index
+ * @n: the number being searched
+ * @index: the respective index of the bit
+ * Return: the bit value.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
@@ -13,9 +12,14 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
-
 	bit_val = (n >> index) & 1;
-
 	return (bit_val);
 }
+/**
+ * This code is used to get the value of a bit at a given specified index. The function takes two parameters, an unsigned long int n and unsigned int index. The function first checks if the index is greater than 63 and if it is, returns -1. If it isn't it then sets the bit_val variable to the result of n shifted right by the index and then and-ed with 1. Finally, the function returns the bit_val variable.
+why does it check if the index is greater than 63
 
+The reason this code checks if the index is greater than 63 is because the maximum number of bits in an unsigned long int is 64, so it's necessary to check that the index isn't greater than the maximum number of bits.
+what is int bit_val?
+
+int bit_val is a variable used to store the value of the bit at the given index.
