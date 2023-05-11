7a50b13134b2e3554e9a1c11cb0c66d11cca69bd
Iggo <Iggonzioki@gmail.com>
2023-05-11 02:15:23 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,3 +26,6 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+/**
+ * This code is used to create a file and write text content to it. It takes two parameters: a const char pointer to the filename (which is the name of the file to create) and a char pointer to the text content which should be written to the file.
+ * The code first checks if the filename is valid (i.e. not NULL). Then it calculates the length of the text content (if it is not NULL). It then creates the file using the open() function with the O_CREAT, O_RDWR, and O_TRUNC flags. The write() function is then used to write the text content to the file, and close() is used to close the file descriptor. Finally, the function returns 1 if the file was created and written successfully, or -1 if it was not.
