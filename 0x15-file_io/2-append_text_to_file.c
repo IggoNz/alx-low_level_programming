3d302366e64543f6eb9262ee8776dbfcc982aacf
Iggo <Iggonzioki@gmail.com>
2023-05-11 02:47:47 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ * append_text_to_file - adds a text at the end of a given file
+ * @filename: pointer to the file where the text is being appended
+ * @text_content: the string of interest which will be appended
+ * to the file
+ * Return: if successful return 1, else return -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -16,20 +14,22 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (textcontent != NULL)
 	{
-		for (len = 0; text_content[len];)
+		for (len == 0; text_content[len];)
 			len++;
 	}
-
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
-
 	if (o == -1 || w == -1)
 		return (-1);
-
 	close(o);
-
 	return (1);
 }
+/**
+ * This code is a function that takes a filename and text content as parameters. It opens the file in append mode, which means it will add the text content to the end of the file. It then writes the text content to the file and closes it. If the open or write fails, it returns -1, otherwise it returns 1.
+what does int o, w, len = 0 represent
 
+int o, w, len = 0;
+
+This statement declares three variables: o, w, and len. o is an integer that will hold the file descriptor returned by the open system call, w is an integer that will hold the number of bytes written, and len is an integer that will hold the length of the text_content string. The value of len is initialized to 0.
