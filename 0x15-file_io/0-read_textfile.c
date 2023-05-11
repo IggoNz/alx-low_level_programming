6b9d9c22b6211d853ed0d4097fd2d75dbcc3e4c9
Iggo <Iggonzioki@gmail.com>
2023-05-11 01:11:47 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,3 +31,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
  * The variables char *buf, ssize_t fd, ssize_t w, and ssize_t t are all used to store information related to the text file. The char *buf stores a pointer to the allocated memory for the file, the ssize_t fd stores the file descriptor, the ssize_t w stores the number of characters written to the screen, and the ssize_t t stores the number of characters read from the file.
  * The code t = read(fd, buff, letters); reads the file and stores the number of characters read in the variable t. The code w = write(STDOUT_FILENO, buf, t); then writes the contents of the file to the screen and stores the number of characters written in the variable w.
  * The code free(buf); frees the memory that was allocated for the file. The code close(fd); closes the file descriptor so that the file is no longer accessible. The code return (w); returns the number of characters that were written to the screen.
+ */
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads the contents of a text file and prints the
+ * contents to POSIX std outpu
+ * @filename: the file whose contents are being read
+ * @letters: the letters in the file name whose contents will be printed out
+ * Return: if successful the bytes being read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t r;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	t = read(buf, letters, fd);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return(w);
+}
